0eaf9ee88c52e76ee3af22bcf6efcf64c9994262
Ruisu005 <Luisacku55@gmail.com>
2024-04-13 16:41:58 -0600

    Update-13/04/2024-Conexion

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,11 +1,5 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "materias_elec");
-
-/*if ($conexion) {
-    echo 'Conectado con exito';
-} else {
-    echo 'No se ha podido conectar';
-}*/
+$conexion = mysqli_connect("localhost:3309", "root", "", "materias_elec");
 
 ?>
